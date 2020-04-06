73e9fa6f74fb24544a0b09dd6327aa3ac3ecd2f7
Nick Danvers <ndanvers222@gmail.com>
2020-04-06 16:45:03 -0700

    testing

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
 
 express()
   .get('/', (req, res) => res.json({"pages":"index"}))
-  .post('/do', (req, res)=> res.send(process.env.SPOON_KEY))
+  .post('/do', (req, res)=> res.json({1:a, b:2, c:3, d:4})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
