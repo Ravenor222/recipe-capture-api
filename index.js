299dfbde24ba73252d8adba6322b952ae222b90b
Nick Danvers <ndanvers222@gmail.com>
2020-04-06 16:56:19 -0700

    testing

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const example = {
+  examples:"thing"
+}
+
 express()
   .get('/', (req, res) => res.json({"pages":"index"}))
-  .post('/do', (req, res)=> {
-    res.json({"pages":"do"})
-  })
+  .post('/', (req, res) => res.json(example))
+  // .post('/do', (req, res)=> {
+  //   res.json({"pages":"do"})
+  // })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
