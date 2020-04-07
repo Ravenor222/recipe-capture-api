13489bf5ce3f661c9ee1b75f28e73c78444c8c6a
Nick Danvers <ndanvers222@gmail.com>
2020-04-06 20:32:05 -0700

    testing

--- index.js
+++ index.js
@@ -18,7 +18,14 @@ const example = {
   //   res.json({"pages":"do"})
   // })
   // .listen(PORT, () => console.log(`Listening on ${ PORT }`))
- app()
-  .get('/', (req, res) => res.json({"pages":"index"}))
-  .post('/', (req, res) => res.json(example))
+
+
+//  app()
+//   .get('/', (req, res) => res.json({"pages":"index"}))
+//   .post('/', (req, res) => res.json(example))
+//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
