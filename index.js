aa77b7c5055798790f9c8b6334fb68f1cedb3b93
Nick Danvers <ndanvers222@gmail.com>
2020-04-06 21:11:10 -0700

    testing

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io').listen(server);
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true, parameterLimit: 100000, limit: '50mb'}));
+app.use(bodyParser.json({limit: '50mb'}))
+app.use(bodyParser.urlencoded({extended: true}));
+
+io.on("connection", socket => {
+  console.log("a user connected ;D");
+
+})
 
 
 app.get('/', (req,res)=> {
